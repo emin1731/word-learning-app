eff41f6ef208fa76f761106259495906347f95cc
emin1731 <emin1731@icloud.com>
2024-08-22 15:23:19 +0400

    Change Recently visited modules' style

--- src/pages/dashboard/homepage/recently-visited.tsx
+++ src/pages/dashboard/homepage/recently-visited.tsx
@@ -26,12 +26,12 @@ const modules: RecentModule[] = [
 export const RecentlyVisited = () => {
   return (
     <div>
-      <h1 className="text-2xl font-semibold text-primary pb-4">
+      <h1 className="text-2xl font-semibold text-primary-foreground pb-4">
         Recent modules
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-20">
         {modules.map((module: RecentModule) => (
-          <div className="h-40 rounded-xl p-5 bg-white drop-shadow-l text-primary">
+          <div className="h-40 rounded-xl p-5 bg-white drop-shadow-l text-primary-foreground">
             <p className="text-xl font-semibold ">{module.name}</p>
             <p>{module.word_count} words</p>
           </div>
