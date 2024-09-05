8776e842ae9853f00ce8690477aea522928c6cc2
emin1731 <emin1731@icloud.com>
2024-09-06 02:02:01 +0400

    Edit Flashcard component

--- src/pages/dashboard/cards/flashcard.tsx
+++ src/pages/dashboard/cards/flashcard.tsx
@@ -1,4 +1,7 @@
+import { Button, buttonVariants } from "@/components/ui/button";
 import { TermDto } from "@/lib/dto/term.dto";
+import { cn } from "@/lib/utils";
+import { Edit, Lightbulb, Star } from "lucide-react";
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 
@@ -14,30 +17,110 @@ export const Flashcard = ({ item, index, numberOfTerms }: FlashcardProps) => {
   return (
     <ReactCardFlip isFlipped={isDefinitionExpanded} flipDirection="horizontal">
       <div
-        className="h-80 p-5 bg-primary text-primary-foreground drop-shadow-l rounded-xl  mb-16 mx-44 cursor-pointer"
+        className="h-96 p-5 bg-primary text-primary-foreground drop-shadow-l rounded-xl  mb-16 mx-44 cursor-pointer"
         onClick={() => setIsDefinitionExpanded(!isDefinitionExpanded)}
       >
         <div className="flex justify-between h-full">
-          <div className="text-xl font-semibold block">term</div>
+          <div className="flex flex-col justify-between text-lg">
+            <div>term</div>
+            <div>
+              {index + 1} of {numberOfTerms}
+            </div>
+          </div>
           <div className="text-3xl font-semibold block self-center">
             {item.term}
           </div>
-          <div className="text-xl font-semibold whitespace-nowrap">
-            {index + 1} of {numberOfTerms}
+          <div className="flex flex-col justify-between items-end text-lg whitespace-nowrap">
+            <div className="flex gap-x-1">
+              <Button
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "customIcon" }),
+                  "h-10 p-2 rounded-full"
+                )}
+              >
+                <Edit size={"20px"} />
+              </Button>
+              <Button
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "customIcon" }),
+                  "h-10 p-2 rounded-full"
+                )}
+              >
+                {item.isStarred ? (
+                  <Star
+                    fontSize="large"
+                    className="self-center cursor-pointer text-yellow-400"
+                    size={"20px"}
+                  />
+                ) : (
+                  <Star
+                    fontSize="large"
+                    className="self-center cursor-pointer"
+                    size={"20px"}
+                  />
+                )}
+              </Button>
+            </div>
+            <div className="">
+              <Button
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "customIcon" }),
+                  "h-10 flex items-center gap-x-1 rounded-full"
+                )}
+              >
+                <Lightbulb size={"20px"} />
+                get a hint
+              </Button>
+            </div>
           </div>
         </div>
       </div>
+
       <div
-        className="h-80 p-5 bg-primary text-primary-foreground drop-shadow-l rounded-xl  mb-16 mx-44 cursor-pointer"
+        className="h-96 p-5 bg-primary text-primary-foreground drop-shadow-l rounded-xl  mb-16 mx-44 cursor-pointer"
         onClick={() => setIsDefinitionExpanded(!isDefinitionExpanded)}
       >
         <div className="flex justify-between h-full">
-          <div className="text-xl font-semibold block">definition</div>
+          <div className="flex flex-col justify-between text-lg">
+            <div>term</div>
+            <div>
+              {index + 1} of {numberOfTerms}
+            </div>
+          </div>
           <div className="text-3xl font-semibold block self-center">
             {item.definition}
           </div>
-          <div className="text-xl font-semibold whitespace-nowrap">
-            {index + 1} of {numberOfTerms}
+          <div className="flex flex-col justify-between items-end text-lg whitespace-nowrap">
+            <div className="flex gap-x-1">
+              <Button
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "customIcon" }),
+                  "h-10 p-2 rounded-full"
+                )}
+              >
+                <Edit size={"20px"} />
+              </Button>
+              <Button
+                className={cn(
+                  buttonVariants({ variant: "ghost", size: "customIcon" }),
+                  "h-10 p-2 rounded-full"
+                )}
+              >
+                {item.isStarred ? (
+                  <Star
+                    fontSize="large"
+                    className="self-center cursor-pointer text-yellow-400"
+                    size={"20px"}
+                  />
+                ) : (
+                  <Star
+                    fontSize="large"
+                    className="self-center cursor-pointer"
+                    size={"20px"}
+                  />
+                )}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
