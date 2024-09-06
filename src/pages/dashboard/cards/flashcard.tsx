4991af769cb1db21ac47de872455552909695c9a
emin1731 <emin1731@icloud.com>
2024-09-06 11:02:31 +0400

    Add flashcard functionality

--- src/pages/dashboard/cards/flashcard.tsx
+++ src/pages/dashboard/cards/flashcard.tsx
@@ -1,9 +1,11 @@
+import { useUpdateTerm } from "@/api/queries/term.queries";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { TermDto } from "@/lib/dto/term.dto";
 import { cn } from "@/lib/utils";
 import { Edit, Lightbulb, Star } from "lucide-react";
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
+import { useParams } from "react-router-dom";
 
 interface FlashcardProps {
   item: TermDto;
@@ -12,13 +14,42 @@ interface FlashcardProps {
 }
 
 export const Flashcard = ({ item, index, numberOfTerms }: FlashcardProps) => {
+  const { moduleId } = useParams();
   const [isDefinitionExpanded, setIsDefinitionExpanded] =
     useState<boolean>(false);
+
+  const { mutateAsync: updateTerm } = useUpdateTerm();
+  const onStarClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    updateTerm({
+      moduleId: moduleId || "",
+      termId: item.id,
+      term: item.term,
+      definition: item.definition,
+      isStarred: !item.isStarred,
+    });
+  };
+  const onCardClick = () => {
+    setIsDefinitionExpanded(!isDefinitionExpanded);
+  };
+
+  const onEditClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.stopPropagation();
+    alert("Flashcard edit clicked");
+  };
+
+  const onGetHintClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    alert("Get hint clicked");
+  };
+
   return (
     <ReactCardFlip isFlipped={isDefinitionExpanded} flipDirection="horizontal">
       <div
         className="h-96 p-5 bg-primary text-primary-foreground drop-shadow-l rounded-xl  mb-16 mx-44 cursor-pointer"
-        onClick={() => setIsDefinitionExpanded(!isDefinitionExpanded)}
+        onClick={() => onCardClick()}
       >
         <div className="flex justify-between h-full">
           <div className="flex flex-col justify-between text-lg">
@@ -33,10 +64,13 @@ export const Flashcard = ({ item, index, numberOfTerms }: FlashcardProps) => {
           <div className="flex flex-col justify-between items-end text-lg whitespace-nowrap">
             <div className="flex gap-x-1">
               <Button
+                id="flashcard-edit-button"
                 className={cn(
                   buttonVariants({ variant: "ghost", size: "customIcon" }),
                   "h-10 p-2 rounded-full"
                 )}
+                datatype="button"
+                onClick={(e) => onEditClick(e)}
               >
                 <Edit size={"20px"} />
               </Button>
@@ -45,6 +79,7 @@ export const Flashcard = ({ item, index, numberOfTerms }: FlashcardProps) => {
                   buttonVariants({ variant: "ghost", size: "customIcon" }),
                   "h-10 p-2 rounded-full"
                 )}
+                onClick={(e) => onStarClick(e)}
               >
                 {item.isStarred ? (
                   <Star
@@ -67,6 +102,7 @@ export const Flashcard = ({ item, index, numberOfTerms }: FlashcardProps) => {
                   buttonVariants({ variant: "ghost", size: "customIcon" }),
                   "h-10 flex items-center gap-x-1 rounded-full"
                 )}
+                onClick={(e) => onGetHintClick(e)}
               >
                 <Lightbulb size={"20px"} />
                 get a hint
